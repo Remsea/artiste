e0862cec9ee2bf18b366d38ba76edb62a27d2824
Rems <remi2202@zohomail.eu>
2019-12-18 18:30:55 +0100

    drag and drop

--- app/javascript/myjs.js
+++ app/javascript/myjs.js
@@ -1,6 +1,59 @@
-const myfunction = () => {
 
-console.log('hello je suis la');
+const checkCapacity = (e) =>
+{ rotate(e);
+  let capa = e.data.dragEvent.data.source.dataset.capacity;
+    if (document.querySelectorAll('.tricontainer.capa'))
+    {
+      const mescibles = document.querySelectorAll('.tricontainer.capa');
+      mescibles.forEach((cible) => {
+        console.log(cible.dataset.capacity, capa);
+
+        if (parseInt(cible.dataset.capacity) < parseInt(capa)) {
+          if (cible !== e.data.startContainer){
+                    cible.classList.remove('tricontainer');
+                    cible.classList.add('notricontainer');
+          }
+        }
+      });
+    }
 }
 
-export {myfunction};
+const rotate = (e) => {console.log(e);
+// e.data.dragEvent.data.source.style.opacity ='0.4';
+// e.data.dragEvent.data.originalSource.style.opacity ='0.1';
+};
+// e.data.dragEvent.data.source.classList.toggle('rotate')
+const removeFormatContainer = (e) => {
+  if (document.querySelectorAll('.notricontainer')){
+    document.querySelectorAll('.notricontainer').forEach(element => {
+        element.className = element.className.replace(/notricontainer/ , 'tricontainer');
+    });
+  }
+  rotate(e);
+}
+
+const updateCapacityContainer = (e) => {
+  if(e.data.newContainer.dataset.capacity){
+      e.data.newContainer.dataset.capacity =parseInt(e.data.newContainer.dataset.capacity) - parseInt(e.data.dragEvent.data.source.dataset.capacity);
+    }
+  if (e.data.oldContainer.dataset.capacity){
+    e.data.oldContainer.dataset.capacity = parseInt(e.data.oldContainer.dataset.capacity) + parseInt(e.data.dragEvent.data.source.dataset.capacity);
+  }
+}
+
+const resizeCapaBlock = (e) => {
+  const size_factor = 3;
+  e.preventDefault();
+  document.querySelectorAll('.atrier').forEach((element) => {
+    element.firstChild.style.width = parseInt(element.dataset.capacity) * size_factor + 'px';
+    element.firstChild.innerText = element.dataset.capacity;
+  });
+
+  document.querySelectorAll('.tricontainer').forEach((element) => {
+    if (element.dataset.capacity)
+      {element.style.width = parseInt(element.dataset.capacity) * size_factor + 'px';}
+  });
+}
+
+
+export {checkCapacity, removeFormatContainer, updateCapacityContainer, resizeCapaBlock, rotate};
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,72 @@
 import "bootstrap";
-import {myfunction} from 'myjs.js';
+import {checkCapacity, removeFormatContainer, updateCapacityContainer, resizeCapaBlock, rotate} from 'myjs.js';
 import { Draggable } from '@shopify/draggable';
-myfunction();
+import { Droppable } from '@shopify/draggable';
+import { Sortable } from '@shopify/draggable';
+import { Plugins } from '@shopify/draggable';
+import SwapAnimation from '@shopify/draggable/lib/plugins/swap-animation';
+// myfunction();
+
+// const draggable = new Draggable(document.querySelectorAll('ul'), {
+//   draggable: 'li',
+//   handle: '.rem'
+// });
+
+// draggable.on('drag:start', (e) => console.log(e.sourceContainer));
+// draggable.on('drag:move', () => console.log('drag:move'));
+// draggable.on('drag:stop', () => console.log('drag:stop'));
+let capacityReached;
+document.querySelector('.start').addEventListener('click', function(e){return resizeCapaBlock(e)})
+
+
+
+const sortable = new Sortable(document.querySelectorAll('.tricontainer'), {
+  draggable: '.atrier'
+});
+
+sortable.on('sortable:start', function(e){return checkCapacity(e)});
+// sortable.on('sortable:sort', () => console.log('sortable:sort', sortable.containers));
+sortable.on('sortable:sorted', (e) => console.log('sorted'));
+sortable.on('sortable:stop', (e) => {
+  removeFormatContainer(e);
+  updateCapacityContainer(e);
+  }
+);
+
+sortable.on('sortable:sort', (evt) => {
+    if (evt.dragEvent.overContainer.classList.contains('notricontainer')) {
+      evt.cancel();
+      }
+    }
+  );
+
+sortable.on('mirror:attached', (evt) => {
+  setTimeout(function(){evt.data.mirror.firstChild.classList.add('rotation');}, 100)
+
+    // evt.data.mirror.firstChild.style.setProperty('transform','rotate(35deg)');
+    console.log(evt.data.mirror.firstChild);
+    console.log(evt.data.mirror.firstElementChild);
+    }
+  );
+
+
+// const droppable = new Droppable(document.querySelectorAll('.container'), {
+//   draggable: '.item',
+//   dropzone: '.dropzone'
+// });
+
+
+
+// const droppable2 = new Droppable(document.querySelectorAll('.box'), {
+//   draggable: '.card',
+//   swapAnimation: {
+//     duration: 600,
+//     easingFunction: 'ease-in-out',
+//     horizontal: true
+//   },
+//   plugins: [Plugins.SwapAnimation],
+//   dropzone: '.dropzone'
+// });
+
+// droppable.on('droppable:dropped', () => console.log('droppable:dropped'));
+// droppable.on('droppable:returned', () => console.log('droppable:returned'));
