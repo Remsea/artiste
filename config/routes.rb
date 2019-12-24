7c4a86d7a687e81a39eb369f8d9f123da0f1618a
Rems <remi2202@zohomail.eu>
2019-12-24 14:42:06 +0100

    changing root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#building'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'building', to: 'pages#building', as: :building
   # get 'contact', to: 'pages#contact', as: :contact
