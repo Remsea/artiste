ad36844d5425baf4b365af9e8f1fbdcf9618ddf6
Rems <remi2202@zohomail.eu>
2020-01-03 14:32:56 +0100

    modif sur l etiquette position et le js associe, modif restriction group avec dataset, modif btn start desactive

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
 import "bootstrap";
-import {start, checkCapacity, removeFormatContainer, updateCapacityContainer, resizeCapaBlock} from './myjs.js';
+import { start, checkCapacity, removeFormatContainer, updateCapacityContainer, resizeCapaBlock } from './myjs.js';
 import { Draggable } from '@shopify/draggable';
 import { Droppable } from '@shopify/draggable';
 import { Sortable } from '@shopify/draggable';
@@ -7,51 +7,49 @@ import { Plugins } from '@shopify/draggable';
 import SwapAnimation from '@shopify/draggable/lib/plugins/swap-animation';
 
 let capacityReached;
-console.log('debut drop');
 
 // initialisation des blocks quand on clique sur le btn start (width fct valeur)
 if (document.querySelector('.start')) {
-document.querySelector('.start').addEventListener('click', function(e){return start(e)});
+    document.querySelector('.start').addEventListener('click', start);
 }
 
 // initialisation de l'objet triable qui se trouve dans une div dropcontainer et avec la class atrier
 const sortable = new Sortable(document.querySelectorAll('.dropcontainer'), {
-  draggable: '.atrierservice'
+    draggable: '.atrierservice'
 });
 
 // Au debut du drag, on verifie les capa des containers
-sortable.on('sortable:start', function(e){return checkCapacity(e)});
+sortable.on('sortable:start', function(e) { return checkCapacity(e) });
 
 // Au stop du drag, on enleve le format rouge des containers avec une capa <
 // et on met à jour les capacités des containers source et cible
 sortable.on('sortable:stop', (e) => {
-  removeFormatContainer(e);
-  updateCapacityContainer(e);
-  }
-);
+    removeFormatContainer(e);
+    updateCapacityContainer(e);
+});
 
 // ne pas rendre possible le drop sur un nodropcontainer, c'est à dire un container avec une capa<
 sortable.on('sortable:sort', (evt) => {
-    if (evt.dragEvent.overContainer.classList.contains('nodropcontainer'))
-      {
+    console.log(evt);
+    if (evt.dragEvent.overContainer.classList.contains('nodropcontainer')) {
         evt.cancel();
-      }
     }
-  );
+    if (evt.dragEvent.overContainer.classList.contains('cardmetier')) {
+        if (evt.dragEvent.overContainer.dataset.group != evt.dragEvent.originalSource.dataset.group) {
+            evt.cancel();
+        }
+    }
+});
 
 // effet animé sur le mirror lors du premier deplacement
 sortable.on('mirror:attached', (evt) => {
-  console.log(evt.data.mirror.firstChild);
-  setTimeout(function(){evt.data.mirror.firstElementChild.classList.add('rotation');}, 100);
-    }
-  );
+    setTimeout(function() { evt.data.mirror.firstElementChild.classList.add('rotation'); }, 100);
+});
 
 // effet animé sur le container si capa insuffisante (nodropcontainer class)
 sortable.on('drag:over:container', (evt) => {
-    if (evt.overContainer.classList.contains('nodropcontainer'))
-      {
+    if (evt.overContainer.classList.contains('nodropcontainer')) {
         evt.overContainer.classList.add('shake-bottom');
-        setTimeout(function(){evt.overContainer.classList.remove('shake-bottom');},1000);
-      }
+        setTimeout(function() { evt.overContainer.classList.remove('shake-bottom'); }, 1000);
     }
-  );
+});
--- app/javascript/packs/myjs.js
+++ app/javascript/packs/myjs.js
@@ -1,157 +1,165 @@
 // verifie les capacites des dropcontainers par rapport au composant draggé et ajoute la class notricontainer si insuffisante
 const checkCapacity = (e) => {
-  let capa = e.data.dragEvent.data.source.dataset.capacity;
-    if (document.querySelectorAll('.dropcontainer.capa'))
-    {
-      const mescibles = document.querySelectorAll('.dropcontainer.capa');
-      mescibles.forEach((cible) => {
-        if (parseInt(cible.dataset.capacity) < parseInt(capa)) {
-          if (cible !== e.data.startContainer){
+    let capa = e.data.dragEvent.data.source.dataset.capacity;
+    if (document.querySelectorAll('.dropcontainer.capa')) {
+        const mescibles = document.querySelectorAll('.dropcontainer.capa');
+        mescibles.forEach((cible) => {
+            if (parseInt(cible.dataset.capacity) < parseInt(capa)) {
+                if (cible !== e.data.startContainer) {
                     cible.classList.remove('dropcontainer');
                     cible.classList.add('nodropcontainer');
-          }
-        }
-      });
+                }
+            }
+        });
     }
 }
 
 // enleve toutes les class nodropcontainer (appelé une fois le drag terminé)
 const removeFormatContainer = (e) => {
-  if (document.querySelectorAll('.nodropcontainer')){
-    document.querySelectorAll('.nodropcontainer').forEach(element => {
-        element.className = element.className.replace(/nodropcontainer/ , 'dropcontainer');
-    });
-  }
+    if (document.querySelectorAll('.nodropcontainer')) {
+        document.querySelectorAll('.nodropcontainer').forEach(element => {
+            element.className = element.className.replace(/nodropcontainer/, 'dropcontainer');
+        });
+    }
 }
 
 // on met à jour les capacités des containers et l'affichage en appelant la fonction updateValuesEtage
 const updateCapacityContainer = (e) => {
-  if(e.data.newContainer.dataset.capacity){
-      e.data.newContainer.dataset.capacity = parseInt(e.data.newContainer.dataset.capacity) - parseInt(e.data.dragEvent.data.source.dataset.capacity);
-      // e.data.newContainer.querySelector('.etiquette-info').firstChild.innerText = e.data.newContainer.dataset.capacity;
-      updateValuesEtage(e.data.newContainer);
+        if (e.data.newContainer.dataset.capacity) {
+            console.log(e.data.newContainer);
+            e.data.newContainer.dataset.capacity = parseInt(e.data.newContainer.dataset.capacity) - parseInt(e.data.dragEvent.data.source.dataset.capacity);
+            // e.data.newContainer.querySelector('.etiquette-info').firstChild.innerText = e.data.newContainer.dataset.capacity;
+            updateValuesEtage(e.data.newContainer);
+        }
+        if (e.data.oldContainer.dataset.capacity) {
+            e.data.oldContainer.dataset.capacity = parseInt(e.data.oldContainer.dataset.capacity) + parseInt(e.data.dragEvent.data.source.dataset.capacity);
+            // e.data.oldContainer.querySelector('.etiquette-info').firstChild.innerText = e.data.oldContainer.dataset.capacity;
+            updateValuesEtage(e.data.oldContainer);
+        }
     }
-  if (e.data.oldContainer.dataset.capacity){
-      e.data.oldContainer.dataset.capacity = parseInt(e.data.oldContainer.dataset.capacity) + parseInt(e.data.dragEvent.data.source.dataset.capacity);
-      // e.data.oldContainer.querySelector('.etiquette-info').firstChild.innerText = e.data.oldContainer.dataset.capacity;
-      updateValuesEtage(e.data.oldContainer);
-    }
+    // fct pour enlever eventlistenner sur le btn start
+const removeEventListenerStart = () => {
+    document.querySelector('.start').removeEventListener('click', start);
+    document.querySelector('.start').classList.add('disabled');
 }
 
 // fonction lancée a partir du btn start qui fait apparaitre les blocs et appelle le dimensionnement
 const start = (e) => {
-  e.preventDefault();
-  const mybuilding = document.querySelector(".inside-building");
-  setTimeout(entranceClass,10);
-  setTimeout(() => {entranceBoucing(mybuilding);}, 600);
-  setTimeout(() => {resizeCapaBlock(e);}, 600);
+    e.preventDefault();
+    const mybuilding = document.querySelector(".inside-building");
+    setTimeout(entranceClass, 10);
+    setTimeout(() => { entranceBoucing(mybuilding); }, 600);
+    setTimeout(() => { resizeCapaBlock(e); }, 600);
 
-  setTimeout(removeEntranceClass, 2000);
-  setTimeout(() => {removeClassBounce(mybuilding);}, 600 + 1200 * mybuilding.children.length);
+    setTimeout(removeEntranceClass, 2000);
+    setTimeout(() => { removeClassBounce(mybuilding); }, 600 + 1200 * mybuilding.children.length);
+    removeEventListenerStart();
 }
+
 
 // effet sur la row (instead of grid) qui apparait (.row instead of grid)
 const entranceClass = () => {
-  document.querySelector('.row').classList.add('swing-in-top-fwd');
+    document.querySelector('.row').classList.add('swing-in-top-fwd');
 }
 
 // supprime la class de l'effet sur grid et remet l'opacité à 1
 const removeEntranceClass = () => {
-  const mygrid = document.querySelector('.row');
-  mygrid.style.opacity = '1';
-  mygrid.classList.remove('swing-in-top-fwd');
+    const mygrid = document.querySelector('.row');
+    mygrid.style.opacity = '1';
+    mygrid.classList.remove('swing-in-top-fwd');
 }
 
 // apparition des etages en bouncing
 const entranceBoucing = (mybuilding) => {
-  let i;
-  let j = mybuilding.children.length;
+    let i;
+    let j = mybuilding.children.length;
 
-  for (i = 0 ; i < mybuilding.children.length; i++)
-  {
-    showUpEtiquetteInfo(mybuilding.children[i], parseInt(mybuilding.children.length));
-    addClassBounce(mybuilding.children[i], j);
-    j--;
-  }
+    for (i = 0; i < mybuilding.children.length; i++) {
+        // showUpEtiquetteInfo(mybuilding.children[i], parseInt(mybuilding.children.length));
+        showUpEtiquetteInfoConstant(mybuilding.children[i]);
+        addClassBounce(mybuilding.children[i].firstElementChild, j);
+        j--;
+    }
 }
 
 // ajout de la class bounce avec un delay sur chaque etage (element est le dropcontainer)
 const addClassBounce = (element, j) => {
-  setTimeout(()=> {
-                element.classList.add('bounce-in-top');
-                element.style.opacity = '1';
-                console.log(500 * j);
-                  }, 500 * j);
+    setTimeout(() => {
+        element.classList.add('bounce-in-top');
+        element.parentNode.style.opacity = '1';
+    }, 500 * j);
 }
 
 //set up de la distance entre l'etage et l etiquette info (element est le dropcontainer)
 //divided by 15 because the element has a css width set to 15em -> update if changes
-const showUpEtiquetteInfo = (element, i) => {
-   setTimeout(()=> {
-    let emvalue = parseInt(element.querySelector('.etiquette-info').offsetWidth) / 14 ;
-    // let offsetvalue = 4 + ((parseInt(element.parentNode.offsetWidth) / parseInt(emvalue)) - (parseInt(element.offsetWidth)/ parseInt(emvalue))) / 2 ;
-    let offsetvalue = 10 * emvalue + ((parseInt(element.parentNode.offsetWidth) ) - (parseInt(element.offsetWidth))) / 2 ;
-   console.log("parent width",element.parentNode.offsetWidth / parseInt(emvalue), 'element width', element.offsetWidth / parseInt(emvalue), 'emvalue', emvalue, 'offsetvalue', offsetvalue );
-    const mediaquery = window.matchMedia("(max-width: 360px)");
-    if (mediaquery.matches) {offsetvalue -= 10;}
-    element.querySelector('.etiquette-info').style.right = -offsetvalue +'px';
-   }, 800 * i);
+// const showUpEtiquetteInfo = (element, i) => {
+//     setTimeout(() => {
+//         let emvalue = parseInt(element.querySelector('.etiquette-info').offsetWidth) / 14;
+//         // let offsetvalue = 4 + ((parseInt(element.parentNode.offsetWidth) / parseInt(emvalue)) - (parseInt(element.offsetWidth)/ parseInt(emvalue))) / 2 ;
+//         let offsetvalue = 10 * emvalue + ((parseInt(element.parentNode.offsetWidth)) - (parseInt(element.offsetWidth))) / 2;
+//         console.log("parent width", element.parentNode.offsetWidth / parseInt(emvalue), 'element width', element.offsetWidth / parseInt(emvalue), 'emvalue', emvalue, 'offsetvalue', offsetvalue);
+//         const mediaquery = window.matchMedia("(max-width: 360px)");
+//         if (mediaquery.matches) { offsetvalue -= 10; }
+//         element.querySelector('.etiquette-info').style.right = -offsetvalue + 'px';
+//     }, 800 * i);
 
+// }
+
+const showUpEtiquetteInfoConstant = (element) => {
+    element.querySelector('.etiquette-info').style.left = '46em';
 }
 
 // supprimer la class bouncing sur les etages
 const removeClassBounce = (mybuilding) => {
-  let i;
-  for (i = 0 ; i < mybuilding.children.length; i++)
-    {mybuilding.children[i].classList.remove('bounce-in-top');}
+    let i;
+    for (i = 0; i < mybuilding.children.length; i++) { mybuilding.children[i].firstElementChild.classList.remove('bounce-in-top'); }
 }
 
 // dimensionnement des blocks
 const resizeCapaBlock = (e) => {
-  const size_factor = 0.50;
-  const size_factor2 = 0.43;
-  let i = 1;
-  document.querySelectorAll('.atrierservice').forEach((element) => {
-    element.firstElementChild.style.width = parseInt(element.dataset.capacity)/10 * size_factor + 'em';
-    updateCapacityPopSurface(element,element.dataset.capacity, i);
-    i++;
-  });
+    const size_factor = 0.50;
+    const size_factor2 = 0.43;
+    let i = 1;
+    document.querySelectorAll('.atrierservice').forEach((element) => {
+        element.firstElementChild.style.width = parseInt(element.dataset.capacity) / 10 * size_factor + 'em';
+        updateCapacityPopSurface(element, element.dataset.capacity, i);
+        i++;
+    });
 
-  document.querySelectorAll('.dropcontainer.capa').forEach((element) => {
-    if (element.dataset.capacity)
-      {
-        element.style.width = parseInt(element.dataset.capacity)/10 * size_factor2 + 'em';
-        updateValuesEtage(element);
-        // element.lastElementChild.innerText = element.dataset.capacity;
-      }
-  });
+    document.querySelectorAll('.dropcontainer.capa').forEach((element) => {
+        if (element.dataset.capacity) {
+            element.style.width = parseInt(element.dataset.capacity) / 10 * size_factor2 + 'em';
+            updateValuesEtage(element);
+            // element.lastElementChild.innerText = element.dataset.capacity;
+        }
+    });
 
 }
 
 //modification des valeurs pour chaque plateau, element est un dropcontainer avec capa
 
 const updateValuesEtage = (element) => {
-  let etiquetteInfo = element.querySelector('.etiquette-info');
-  let surfaceUtilise = parseInt(element.dataset.originalcapacity) - parseInt(element.dataset.capacity);
-  let surfaceOriginal = element.dataset.originalcapacity;
-  if (surfaceUtilise == surfaceOriginal) {element.querySelector('.empty').style.opacity = 0;}
-  else {element.querySelector('.empty').style.opacity = 1;}
-  Array.from(etiquetteInfo.children).forEach((etiquetteChild) => {
-                        etiquetteChild.classList.contains('pop-pourcentage') ? (etiquetteChild.innerText = (Math.round(parseInt(surfaceUtilise) / parseInt(surfaceOriginal) * 100)) + '%' ) : '';
-                        etiquetteChild.classList.contains('pop-surface-utilise') ? (etiquetteChild.innerText = surfaceUtilise) : '';
-                        etiquetteChild.classList.contains('surface-total') ? (etiquetteChild.innerText = '/ ' + surfaceOriginal + 'm2')  : '';
-                          });
+    const parentcontainer = element.parentNode;
+    let etiquetteInfo = parentcontainer.querySelector('.etiquette-info');
+    let surfaceUtilise = parseInt(element.dataset.originalcapacity) - parseInt(element.dataset.capacity);
+    let surfaceOriginal = element.dataset.originalcapacity;
+    if (surfaceUtilise == surfaceOriginal) { element.querySelector('.empty').style.opacity = 0; } else { element.querySelector('.empty').style.opacity = 1; }
+    Array.from(etiquetteInfo.children).forEach((etiquetteChild) => {
+        etiquetteChild.classList.contains('pop-pourcentage') ? (etiquetteChild.innerText = (Math.round(parseInt(surfaceUtilise) / parseInt(surfaceOriginal) * 100)) + '%') : '';
+        etiquetteChild.classList.contains('pop-surface-utilise') ? (etiquetteChild.innerText = surfaceUtilise) : '';
+        etiquetteChild.classList.contains('surface-total') ? (etiquetteChild.innerText = '/ ' + surfaceOriginal + 'm2') : '';
+    });
 }
 
 //affichage des surfaces par service dans block pop-surface
 
 const updateCapacityPopSurface = (element, surface, i) => {
-  setTimeout(()=> {
-    element.querySelector('.pop-surface').innerText = surface + ' m2';
-    element.querySelector('.pop-surface').style.opacity = 1;
+    setTimeout(() => {
+        element.querySelector('.pop-surface').innerText = surface + ' m2';
+        element.querySelector('.pop-surface').style.opacity = 1;
     }, 2000 + 400 * parseInt(i));
 }
 
 
 
-export {checkCapacity, removeFormatContainer, updateCapacityContainer, resizeCapaBlock, start};
+export { checkCapacity, removeFormatContainer, updateCapacityContainer, resizeCapaBlock, start };
